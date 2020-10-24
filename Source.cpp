c840a749e095d20b83c500934b8fb7c2816be980
klorman <38407912+klorman@users.noreply.github.com>
2020-10-24 22:05:51 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,28 +6,20 @@
 
 RGBQUAD* Video_memory = NULL;
 
-const int SSAA = 2;
+const int SSAA = 1;
 
 struct point {
 	double x, y, z;
 };
 
 class CoordSys {
-private:
-	point coords0_, coords1_;
-	double scaleX_, scaleY_;
-
 public:
+	point size_;
 	void draw_pixel(point coords, point color_of_point);
-	void draw_point(point coords);
-	void draw_line(point coords0, point coords1);
 	point to_pixels(point coords);
 
-	CoordSys(point coords0, point coords1, double scaleX, double scaleY) {
-		coords0_ = coords0;
-		coords1_ = coords1;
-		scaleX_ = scaleX;
-		scaleY_ = scaleY;
+	CoordSys(point size) {
+		size_ = size;
 	}
 };
 
@@ -36,12 +28,31 @@ public:
 	point coords_;
 	double length();
 	void normalization();
-	void draw_vector(point start_coords, CoordSys& vector_space);
 
-	Vector(point coords) {
-		coords_.x = coords.x;
-		coords_.y = coords.y;
-		coords_.z = coords.z;
+	Vector(point coords = { 0, 0, 0 }) {
+		coords_ = coords;
+	}
+};
+
+class Light_source {
+public:
+	Vector light_source_;
+	Vector light_source_color_;
+	void draw_light_source(CoordSys& vector_space, double R);
+
+	Light_source(Vector light_source, Vector light_source_color) {
+		light_source_ = light_source;
+		light_source_color_ = light_source_color;
+		++count();
+	}
+
+	~Light_source() {
+		--count();
+	}
+
+	size_t& count() {
+		static size_t c = 0;
+		return c;
 	}
 };
 
@@ -86,78 +97,17 @@ void Vector::normalization() {
 	coords_.z = temp_vector.coords_.z;
 }
 
-void Vector::draw_vector(point start_coords, CoordSys& vector_space) {
-	point end_coords = { coords_.x + start_coords.x, coords_.y + start_coords.y };
-
-	vector_space.draw_line(start_coords, end_coords);
-	vector_space.draw_point(start_coords);
-
-	Vector vector_for_arrow1({ -coords_.y ,  coords_.x });
-	Vector vector_for_arrow2({ coords_.y , -coords_.x });
-	Vector reverse = -1 * *this;
-
-	reverse.normalization();
-	vector_for_arrow1.normalization();
-	vector_for_arrow2.normalization();
-
-	Vector arrow1 = reverse + vector_for_arrow1;
-	Vector arrow2 = reverse + vector_for_arrow2;
-
-	arrow1.normalization();
-	arrow2.normalization();
-
-	point end_of_arrow1 = { arrow1.coords_.x + end_coords.x, arrow1.coords_.y + end_coords.y };
-	vector_space.draw_line(end_coords, end_of_arrow1);
-
-	point end_of_arrow2 = { arrow2.coords_.x + end_coords.x, arrow2.coords_.y + end_coords.y };
-	vector_space.draw_line(end_coords, end_of_arrow2);
-}
-
 point CoordSys::to_pixels(point coords) {
-	point start_of_coord = { coords1_.x / 2, coords1_.y / 2 };
-	point rec_coord;
+	point start_of_coord = { size_.x / 2, size_.y / 2 };
 
-	rec_coord.x = coords.x * scaleX_ + start_of_coord.x;
-	rec_coord.y = start_of_coord.y - coords.y * scaleY_;
-
-	return rec_coord;
-}
-
-void CoordSys::draw_point(point coords) {
-	txSetColor(TX_BLACK);
-	txSetFillColor(TX_BLACK);
-
-	point rec_coord = to_pixels(coords);
-
-	if (rec_coord.y >= coords0_.y &&
-		rec_coord.y <= coords1_.y &&
-		rec_coord.x <= coords1_.x &&
-		rec_coord.x >= coords0_.x)
-		txCircle(rec_coord.x, rec_coord.y, 2);
-}
-
-void CoordSys::draw_line(point coords0, point coords1) {
-	txSetColor(TX_BLUE);
-
-	point rec_coord0 = to_pixels(coords0);
-	point rec_coord1 = to_pixels(coords1);
-
-	if (rec_coord0.y >= coords0_.y &&
-		rec_coord0.y <= coords1_.y &&
-		rec_coord0.x <= coords1_.x &&
-		rec_coord0.x >= coords0_.x &&
-		rec_coord1.y >= coords0_.y &&
-		rec_coord1.y <= coords1_.y &&
-		rec_coord1.x <= coords1_.x &&
-		rec_coord1.x >= coords0_.x)
-		txLine(rec_coord0.x, rec_coord0.y, rec_coord1.x, rec_coord1.y);
+	return point({ coords.x + start_of_coord.x, start_of_coord.y - coords.y });
 }
 
 void CoordSys::draw_pixel(point coords, point color_of_point) {
 	//txSetFillColor(color_of_point);
 	point rec_coords = to_pixels(coords);
 	//txSetPixel(rec_coords[0], rec_coords[1], color_of_point);
-	RGBQUAD* pixel = &Video_memory[(700 - (int)rec_coords.y) * 700 + (int)rec_coords.x];
+	RGBQUAD* pixel = &Video_memory[((int)size_.x - (int)rec_coords.y) * (int)size_.y + (int)rec_coords.x];
 	pixel->rgbRed = color_of_point.x;
 	pixel->rgbGreen = color_of_point.y;
 	pixel->rgbBlue = color_of_point.z;
@@ -176,135 +126,154 @@ Vector get_circle(Vector& n, Vector& O1, Vector color_of_ball, Vector new_color,
 	return color_of_ball;
 }
 
-Vector get_color(CoordSys& vector_space, Vector& n, Vector& light_source, Vector& light_source_color,int R) {
+Vector get_color(CoordSys& vector_space, Vector& n, Vector& observer, Light_source& source, Vector& color_of_ball, int R) {
 
 	if (n.length() <= R) {
 		double z = sqrt(R * R - n.coords_.x * n.coords_.x - n.coords_.y * n.coords_.y);
 		n.coords_.z = z;
 
-		//n.coords_.x += sin(100 * n.coords_.z) * 50;
+		//n.coords_.x += sin(10000 * n.coords_.x + 100000000) * 10;
 		//n.coords_.y += cos(10 * n.coords_.z) * 500;
-		//n.coords_.z += sin(0.1 * n.coords_.x) * 1000;
+		//n.coords_.z += sin(10000 * n.coords_.z + 100000000) * 10;
 
-		Vector color_of_ball({ 128, 64, 64 });
 
 		Vector O1({ -120, 0, 120 });
 		O1 = SSAA * O1;
 
-		color_of_ball = get_circle(n, O1, color_of_ball, Vector({ 255, 255, 255 }), SSAA * 50, z);
+
+		Vector color_of_point = color_of_ball;
+
+		color_of_point = get_circle(n, O1, color_of_point, Vector({ 255, 255, 255 }), SSAA * 50, z);
 
 		Vector O2 = O1 + (SSAA * Vector({ -1, 11, 1 }));
-		color_of_ball = get_circle(n, O2, color_of_ball, Vector({ 0, 0, 0 }), SSAA * 22, z);
-		color_of_ball = get_circle(n, O2, color_of_ball, Vector({ 250, 250, 250 }), SSAA * 18, z);
+		color_of_point = get_circle(n, O2, color_of_point, Vector({ 0, 0, 0 }), SSAA * 22, z);
+		color_of_point = get_circle(n, O2, color_of_point, Vector({ 250, 250, 250 }), SSAA * 18, z);
 
 		O2 = O1 + (SSAA * Vector({ -1, -11, 1 }));
-		color_of_ball = get_circle(n, O2, color_of_ball, Vector({ 0, 0, 0 }), SSAA * 22, z);
-		color_of_ball = get_circle(n, O2, color_of_ball, Vector({ 250, 250, 250 }), SSAA * 18, z);
+		color_of_point = get_circle(n, O2, color_of_point, Vector({ 0, 0, 0 }), SSAA * 22, z);
+		color_of_point = get_circle(n, O2, color_of_point, Vector({ 250, 250, 250 }), SSAA * 18, z);
 
-		//O2 = O1 + (SSAA * Vector({ -10, -11, -3 }));
-		//color_of_ball = get_circle(n, O2, color_of_ball, Vector({ 250, 250, 250 }), SSAA * 22, z);
-
-
-		Vector L = light_source + (-1 * n);
+		Vector L = source.light_source_ + (-1 * n);
 
 		double brightness = (L ^ n) / (L.length() * n.length());
 
 		if (brightness < 0) brightness = 0;
 
-		Vector ambient_lighting({ 255, 255, 255 });
 
-		Vector observer({ 0, 0, 350 });
+
 		Vector L1 = 2 * L.length() * brightness * (1. / n.length() * n) + ((-1) * L);
 
 		double k = 50;
 		double cos_beta = (observer ^ L1) / (observer.length() * L1.length()), sin_beta = 1 * sqrt(1 - pow((cos_beta), 2));
+
 		
-		if (cos_beta < 0 || light_source.coords_.z < 0) cos_beta = 0;
+		if (cos_beta < 0 || source.light_source_.coords_.z < -150 || source.light_source_.length() <= R) cos_beta = 0;
 
 		double I = pow(cos_beta, k);
 
-		Vector lambert = 255 * (brightness * 1. / 255 * color_of_ball) * (1. / 255 * light_source_color);
-		Vector ambient = 255 * ((0.04 / 255 * ambient_lighting) * (1. / 255 * color_of_ball));
-
-		color_of_ball = lambert + ambient + I * light_source_color;
 		
-		return Vector({MIN(color_of_ball.coords_.x, 255),
-					   MIN(color_of_ball.coords_.y, 255),
-					   MIN(color_of_ball.coords_.z, 255)});
+		Vector ambient_lighting_color({ 255, 255, 255 });
+
+		Vector ambient = 1. / source.count() * 255 * ((0.04 / 255 * ambient_lighting_color) * (1. / 255 * color_of_point));
+		Vector lambert = 255 * (brightness * 1. / 255 * color_of_point) * (1. / 255 * source.light_source_color_);
+
+
+		color_of_point = ambient + lambert + I * source.light_source_color_;
+		
+		return color_of_point;
 	}
 
 	return Vector({ 0, 0, 0 });
 }
 
-void draw_light_source(Vector& light_source_color, Vector& light_source) {
-	txSetFillColor(RGB(light_source_color.coords_.x,
-		light_source_color.coords_.y,
-		light_source_color.coords_.z));
+void Light_source::draw_light_source(CoordSys& vector_space, double R) {
+	light_source_ = 1. / SSAA * light_source_;
 
-	txSetColor(RGB(light_source_color.coords_.x,
-		light_source_color.coords_.y,
-		light_source_color.coords_.z));
+	double r = 8 * (light_source_.coords_.z + 450) / 800;
 
-	if (light_source.coords_.x > -350,
-		light_source.coords_.x < 350,
-		light_source.coords_.y > -350,
-		light_source.coords_.y < 350) txCircle(350 + light_source.coords_.x, 350 - light_source.coords_.y, 8 * (light_source.coords_.z + 450.) / 800);
+	for (double x = light_source_.coords_.x - r; x < light_source_.coords_.x + r; x++) {
+		for (double y = light_source_.coords_.y - r; y < light_source_.coords_.y + r; y++) {
 
+			if (x > -350 &&
+				x < 350 &&
+				y > -350 &&
+				y < 350 &&
+				(-1 * Vector({ x, y, 0 }) + Vector({ light_source_.coords_.x, light_source_.coords_.y, 0 })).length() <= r &&
+				(Vector({ x, y, 0 }).length() >= R || light_source_.coords_.z >= 0))
+				vector_space.draw_pixel(point({ x, y, 0 }), light_source_color_.coords_);
+		}
+	}
 }
 
-void draw_object(double rotation) {
-	txSetFillColor(TX_BLACK);
+void draw_object(CoordSys& vector_space, double rotation) {
 	txClear();
 
 	double R = 155;
-	CoordSys vector_space({ 0, 0 }, { 700, 700 }, 1, 1);
-	Vector light_source({ -320, 120, 320 });
-	Vector light_source_color({ 255, 0, 255 });
 
-	light_source.coords_.x *= cos(rotation);
-	light_source.coords_.z *= sin(rotation);
+	Light_source purple(Vector({ -320, 120, 320 }), Vector({ 255, 0, 255 }));
+	Light_source rainbow(Vector({ 150, 320, 220 }), Vector({ 255, 255, 255 }));
+	Vector observer({ 0, 0, 350 });
 
-	for (double y = -160; y < 160; y++) {
+	Vector color_of_ball({ 128, 64, 64 });
+
+	observer = SSAA * observer;
+	purple.light_source_  = SSAA * purple.light_source_;
+	rainbow.light_source_ = SSAA * rainbow.light_source_;
+
+	purple.light_source_.coords_.x *= cos(rotation);
+	purple.light_source_.coords_.z *= sin(rotation);
+
+
+	rainbow.light_source_color_.coords_.x *= abs(sin(rotation));
+	rainbow.light_source_color_.coords_.y *= abs(sin(1.5 * rotation));
+	rainbow.light_source_color_.coords_.z *= abs(sin(3 * rotation));
+
+	rainbow.light_source_.coords_.x *= cos(0.5 * rotation) * cos(rotation);
+	rainbow.light_source_.coords_.z *= cos(0.5 * rotation);
+	rainbow.light_source_.coords_.y *= sin(0.5 * rotation);
+
+
+	for (double y = -160; y < 160; y++)
 		for (double x = -160; x < 160; x++) {
 			Vector n({ x, y, 0 });
-			Vector color_of_ball({ 0, 0, 0 });
-
-			Vector light_source1 = SSAA * light_source;
-			n = SSAA * n;
+			Vector color_of_point({ 0, 0, 0 });
 
 			for (double x1 = 0; x1 < SSAA; x1++)
 				for (double y1 = 0; y1 < SSAA; y1++) {
-					Vector n1 = n + Vector({ x1, y1, 0 });
+					Vector n1 = SSAA * n + Vector({ x1, y1, 0 });
 					
-					color_of_ball = color_of_ball + 1. / (SSAA * SSAA) * get_color(vector_space, n1, light_source1, light_source_color, R * SSAA);
+					color_of_point = color_of_point + 1. / (SSAA * SSAA) * get_color(vector_space, n1, observer, purple, color_of_ball, R * SSAA);		
+					color_of_point = color_of_point + 1. / (SSAA * SSAA) * get_color(vector_space, n1, observer, rainbow, color_of_ball, R * SSAA);
+
+					color_of_point = Vector({ MIN(color_of_point.coords_.x, 255),
+											  MIN(color_of_point.coords_.y, 255),
+											  MIN(color_of_point.coords_.z, 255) });
 				}
 
-			n = 1. / SSAA * n;
-
-			if (color_of_ball.length() != 0) vector_space.draw_pixel(point({ n.coords_.x, n.coords_.y, n.coords_.z }), point({ color_of_ball.coords_.x, color_of_ball.coords_.y, color_of_ball.coords_.z }));
+			if (color_of_point.length() != 0) vector_space.draw_pixel(n.coords_, color_of_point.coords_);
 		}
-	}
 
-	if (Vector({ light_source.coords_.x, light_source.coords_.y, 0}).length() >= R + 8 || light_source.coords_.z >= 0) draw_light_source(light_source_color, light_source);
+	purple.draw_light_source(vector_space, R);
+	rainbow.draw_light_source(vector_space, R);
 }
 
 int main() {
-	txCreateWindow(700, 700);
-
+	CoordSys vector_space({ 700, 700 });
+	txCreateWindow(vector_space.size_.x, vector_space.size_.y);
+	txSetFillColor(TX_BLACK);
+	
 	Video_memory = txVideoMemory();
 
 	txBegin();
 
-	double rotation = 0;
 
-
-
-	for (rotation; !txGetAsyncKeyState(VK_ESCAPE);) {
+	for (double rotation = 0; !txGetAsyncKeyState(VK_ESCAPE);) {
 		double fps = txGetFPS();
 
 		if (fps != 0) rotation += 1 / fps;
+		if (rotation >= 4 * txPI) rotation -= 4 * txPI;
 
-		draw_object(rotation);
+		draw_object(vector_space, rotation);
 
 		std::cout << fps << "\n";
 		txSleep();
